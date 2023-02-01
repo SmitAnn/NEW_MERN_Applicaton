bfb79380808ce9022445b4f43fa0e15d41ece82c
SmitAnn <117157995+SmitAnn@users.noreply.github.com>
2023-02-02 00:43:32 +0300

    init file

---
+++ Models/employee.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const EmployeeSchema = mongoose.Schema(
+    {
+        name: String,
+        position: String,
+        location: String,
+        salary: String
+    }
+);
+var EmployeeModel = mongoose.model('employees', EmployeeSchema);
+module.exports = { EmployeeModel }
